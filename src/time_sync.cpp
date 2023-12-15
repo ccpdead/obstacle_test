6c8d4efb4de3f25911b5ef72fb885d345e981ccf
yelume <yelume@163.com>
2023-12-15 16:18:02 +0800

    添加点云滤波程序

--- src/time_sync.cpp
+++ src/time_sync.cpp
@@ -10,6 +10,17 @@
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 
+#include <pcl/filters/passthrough.h>
+#include<pcl/filters/crop_box.h>
+
+// 定义ROI区域
+float max_x = 15.0;
+float min_x = -3.0;
+float max_y = 3.0;
+float min_y = -3.0;
+float max_z = 3;
+float min_z = -1;
+
 std::string lidar1_topic = "/mid360_front";
 std::string lidar2_topic = "/mid360_back";
 
@@ -21,32 +32,59 @@ typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2
 
 // 回调函数
 void callback(const sensor_msgs::PointCloud2ConstPtr& lidar1, const sensor_msgs::PointCloud2ConstPtr& lidar2) {
-    // 事实发布的话题
-    sync_lidar1.publish(*lidar1);
-    sync_lidar2.publish(*lidar2);
-    static int i;
-    printf("times:%d\r\n", i++); 
-
+    // // 事实发布的话题
+    // sync_lidar1.publish(*lidar1);
+    // sync_lidar2.publish(*lidar2);
+    // static int i;
+    // printf("times:%d\r\n", i++);
 
     // 合并点云
-    pcl::PointCloud<pcl::PointXYZ> pclCloud1;
+    pcl::PointCloud<pcl::PointXYZI> pclCloud1;
     pcl::fromROSMsg(*lidar1, pclCloud1);
-    
-    pcl::PointCloud<pcl::PointXYZ> pclCloud2;
+
+    pcl::PointCloud<pcl::PointXYZI> pclCloud2;
     pcl::fromROSMsg(*lidar2, pclCloud2);
 
     // 合并点云数据
-    pcl::PointCloud<pcl::PointXYZ> mergedCloud;
+    pcl::PointCloud<pcl::PointXYZI> mergedCloud;
+    pcl::PointCloud<pcl::PointXYZI> pcl_roi;
     mergedCloud += pclCloud1;
     mergedCloud += pclCloud2;
 
+    // ROI区域分割
+    pcl::PassThrough<pcl::PointXYZI> pass;
+    pass.setInputCloud(mergedCloud.makeShared());
+    pass.setFilterLimitsNegative(false);
+    pass.setFilterFieldName("x");
+    pass.setFilterLimits(min_x, max_x);
+    pass.filter(pcl_roi);
+
+    pass.setInputCloud(pcl_roi.makeShared());
+    pass.setFilterFieldName("y");
+    pass.setFilterLimits(min_y, max_y);
+    pass.filter(pcl_roi);
+
+    pass.setInputCloud(pcl_roi.makeShared());
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(min_z, max_z);
+    pass.filter(pcl_roi);
+
+
+    // 车身点云剔除
+    pcl::CropBox<pcl::PointXYZI> crop;
+    crop.setInputCloud(pcl_roi.makeShared());
+    crop.setMin(Eigen::Vector4f(-1.35, -0.75, -0.5, 1.0)); // Set the minimum point of the crop box
+    crop.setMax(Eigen::Vector4f(1.35, 0.75, 0.5, 1.0));   // Set the maximum point of the crop box
+    crop.setNegative(true); // Set to true to remove points inside the box
+    crop.filter(pcl_roi);
+
+
     // 将合并后的点云数据发布到lidar_fusion话题
     sensor_msgs::PointCloud2 fusedCloud;
-    pcl::toROSMsg(mergedCloud, fusedCloud);
+    pcl::toROSMsg(pcl_roi, fusedCloud);
     fusedCloud.header.frame_id = "mid360_frame";
     lidar_fusion.publish(fusedCloud);
     printf("Merged point clouds and published to lidar_fusion topic.\n");
-
 }
 
 int main(int argc, char** argv) {
