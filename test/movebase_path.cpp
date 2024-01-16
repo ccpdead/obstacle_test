be2830f14c4948bc1cd740b830d76982f6002725
yelume <yelume@163.com>
2024-01-16 08:54:20 +0800

    tfbuffer测试

--- test/movebase_path.cpp
+++ test/movebase_path.cpp
@@ -8,9 +8,9 @@
 #include <nav_msgs/Path.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include <iostream>
 #include <vector>
@@ -23,7 +23,7 @@
 
 class movebase_path {
    public:
-    movebase_path(ros::NodeHandle &nh) {
+    movebase_path(ros::NodeHandle nh) {
         subPath = nh.subscribe("/move_base/local_plan", 1, &movebase_path::local_plan_callback, this);
         pubPath = nh.advertise<sensor_msgs::PointCloud2>("car_path", 1);
     };
@@ -31,21 +31,24 @@ class movebase_path {
 
    public:
     void local_plan_callback(const nav_msgs::Path::ConstPtr& msg) {
-        tf2_ros::Buffer tfBuffer;  // 使用tf进行坐标转换
-        tf2_ros::TransformListener tfListener(tfBuffer);
-
         // 保存转换坐标后的路径数据
-        std::vector<geometry_msgs::Point> transformed_points = transformPathToBaseLink(msg, tfBuffer);
+        std::vector<geometry_msgs::Point> transformed_points = transformPathToBaseLink(msg);
 
         publishPointCloud(transformed_points);
     }
 
-    std::vector<geometry_msgs::Point> transformPathToBaseLink(const nav_msgs::Path::ConstPtr& msg, tf2_ros::Buffer& tf_buffer) {
+    std::vector<geometry_msgs::Point> transformPathToBaseLink(const nav_msgs::Path::ConstPtr& msg) {
+        tf2_ros::Buffer tfBuffer;                  // 使用tf进行坐标转换
+        tf2_ros::TransformListener tfListener(tfBuffer);  // 启动监听
         std::vector<geometry_msgs::Point> transformed_points;
         for (const auto& pose_stamped : msg->poses) {
+            printf("pose_stamped.header.frame_id:%s\n", pose_stamped.header.frame_id.c_str());
+            printf("pose_stamped.pose.position.x:%f\n", pose_stamped.pose.position.x);
+            printf("pose_stamped.pose.position.y:%f\n", pose_stamped.pose.position.y);
+            printf("pose_stamped.pose.position.z:%f\n", pose_stamped.pose.position.z);
             geometry_msgs::PoseStamped pose_stamped_base_link;
             try {
-                tf_buffer.transform(pose_stamped, pose_stamped_base_link, "base_link");
+                tfBuffer.transform(pose_stamped, pose_stamped_base_link, "base_link");
             } catch (tf2::TransformException& ex) {
                 ROS_WARN("%s", ex.what());
                 continue;
@@ -65,19 +68,10 @@ class movebase_path {
 
         for (const auto& data : points) {
             pcl::PointXYZ point;
-            pcl::PointXYZ point_left;
-            pcl::PointXYZ point_right;
             point.x = data.x;
             point.y = data.y;
             point.z = data.z;
-
-            point_left = point_right = point;
-            point_left.x = point.x - 0.2;
-            point_right.x = point.x + 0.2;
-
             cloud_pcl->push_back(point);
-            cloud_pcl->push_back(point_left);
-            cloud_pcl->push_back(point_right);
         }
 
         pcl::toROSMsg(*cloud_pcl, point_cloud_msg);
@@ -86,7 +80,6 @@ class movebase_path {
 
    private:
     nav_msgs::Path::ConstPtr path;
-
     ros::Subscriber subPath;  // 订阅路径
     ros::Publisher pubPath;   // 发布路径
 };
